5c25775877e12515992cec7e2da2636bf0a60f91
Appleeway <applee62002@gmail.com>
2020-03-15 22:17:44 +0800

    Add UI messages

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')
 
 
 // set connection to mongoDB
@@ -50,6 +51,9 @@ app.use(session({
   saveUninitialized: true,
 }))
 
+// 使用connect-flash
+app.use(flash())
+
 // 使用 Passport 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -61,6 +65,8 @@ require('./config/passport')(passport)
 app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/user.js
+++ routes/user.js
@@ -84,7 +84,7 @@ router.post('/register', (req, res) => {
 // 登出
 router.get('/logout', (req, res) => {
   req.logout()
-  // req.flash('success_msg', '你已經成功登出')
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
